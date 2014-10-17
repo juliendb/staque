6fcfd600f9f775b20160b366b164904d9d68b66c
guillaume <julien.debisschop@gmail.com>
2014-10-17 12:21:28 +0200

    corrections

---
+++ inc/bottom.php
@@ -0,0 +1,12 @@
+	
+		<footer id="footer">
+			<?php date_default_timezone_set('Europe/London'); ?>
+			<p>&copy; <?php echo date('Y'); ?> staque juga</p>
+		</footer>
+
+
+	<script src="js/jquery-min.js"></script>
+	<script src="js/script.js"></script>
+	
+</body>
+</html>
