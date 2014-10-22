7d4769d0aa74ac4dfe4aa3cdee867741be272bf8
guillaume <julien.debisschop@gmail.com>
2014-10-22 17:38:41 +0200

    encore

--- inc/top.php
+++ inc/top.php
@@ -11,6 +11,7 @@
 	<title>staque juga</title>
 	<meta name="description" content="">
 	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400' rel='stylesheet' type='text/css'>
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <div id="wrapper">
